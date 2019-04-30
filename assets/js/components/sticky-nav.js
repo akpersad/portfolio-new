5ca22f5d1f6ce942b67e9bbd82e466c16d00768d
Andrew Persad <apersad@deloitte.com>
2019-04-30 00:20:22 -0400

    Test

--- assets/js/components/sticky-nav.js
+++ assets/js/components/sticky-nav.js
@@ -5,6 +5,7 @@ const navHeight = nav.offsetHeight;
 window.addEventListener("scroll", function(event) {
 	const curPos = this.pageYOffset;
 	const len = sections.length;
+	const element = event.target;
 	for (let i = 0; i < len; i++) {
 		const top = sections[i].offsetTop - navHeight;
 		const bottom = top + sections[i].offsetHeight;
@@ -13,10 +14,12 @@ window.addEventListener("scroll", function(event) {
 			console.log(sections[i]);
 			const anchorTags = nav.querySelectorAll("a");
 			for (let j = 0; j < anchorTags.length; j++) {
-				const element = anchorTags[j];
-				element.classList.remove("active");
+				anchorTags[j].classList.remove("active");
 			}
 			document.querySelector(`a.${sections[i].id}`).classList.add("active");
 		}
+		// else if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+		// 	console.log("Hello");
+		// }
 	}
 });
