a2483e0fecd864de22ff8f7f2cad882d6231d2f2
Andrew Persad <apersad@deloitte.com>
2019-05-01 13:37:04 -0400

    House keeping plus scroll at bottom

--- main/assets/js/dist/sticky-nav.js
+++ main/assets/js/dist/sticky-nav.js
@@ -13,6 +13,17 @@ var stickykNav = function stickykNav() {
 	}
 };
 
+var isScrolledBottom = function isScrolledBottom() {
+	var pageHeight = document.documentElement.offsetHeight;
+	var windowHeight = window.innerHeight;
+	var scrollPosition = window.scrollY || window.pageYOffset || document.body.scrollTop + (document.documentElement && document.documentElement.scrollTop || 0);
+
+	if (pageHeight <= windowHeight + scrollPosition) {
+		return true;
+	}
+	return false;
+};
+
 window.addEventListener("scroll", function (event) {
 	var curPos = this.pageYOffset;
 	var len = sections.length;
@@ -20,18 +31,16 @@ window.addEventListener("scroll", function (event) {
 	for (var i = 0; i < len; i++) {
 		var top = sections[i].offsetTop - navHeight;
 		var bottom = top + sections[i].offsetHeight;
+		var anchorTags = nav.querySelectorAll("a");
 
 		if (curPos >= top && curPos <= bottom) {
-			console.log(sections[i]);
-			var anchorTags = nav.querySelectorAll("a");
 			for (var j = 0; j < anchorTags.length; j++) {
 				anchorTags[j].classList.remove("active");
 			}
 			document.querySelector("a." + sections[i].id).classList.add("active");
+		} else if (isScrolledBottom()) {
+			anchorTags[len - 2].classList.remove("active");
+			document.querySelector("a." + sections[len - 1].id).classList.add("active");
 		}
-		// else if (element.scrollHeight - element.scrollTop === element.clientHeight) {
-		// 	console.log("Hello");
-		// document.querySelector('.sections').scrollHeight - document.querySelector('.sections').scrollTop === document.querySelector('.sections').clientHeight
-		// }
 	}
 });
